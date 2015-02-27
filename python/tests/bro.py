a40e592346e8e4a0f029440633386e6d57a9eaa7
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-02-27 18:40:32 +0000

    [bro.py] do decompression in a loop if output length is not specified or it cannot be calculated in advance by decoder

--- python/tests/bro.py
+++ python/tests/bro.py
@@ -2,15 +2,15 @@
 """bro %s -- compression/decompression utility using the Brotli algorithm."""
 
 from __future__ import print_function
-import brotli
 import getopt
 import sys
 import os
+import brotli
 
 
 __usage__ = """\
 Usage: bro [--force] [--decompress] [--input filename] [--output filename]
-    [--mode 'text'|'font'] [--transform] [--bufsize]"""
+    [--mode 'text'|'font'] [--transform] [--length <int>] [--bufsize <int>]"""
 
 __version__ = '0.1'
 
@@ -20,35 +20,51 @@ BROTLI_MODES = {
     'font': brotli.MODE_FONT
 }
 
+MAX_BUFFER_SIZE = 1000000000  # 1 GB
 
-def get_binary_stdout():
+
+def get_binary_stdio(stream):
+    """ Return the specified standard input, output or errors stream as a
+    'raw' buffer object suitable for reading/writing binary data from/to it.
+    """
+    assert stream in ['stdin', 'stdout', 'stderr'], "invalid stream name"
+    stdio = getattr(sys, stream)
     if sys.version_info[0] < 3:
         if sys.platform == 'win32':
-            # set stdout binary flag on python2.x (Windows)
+            # set I/O stream binary flag on python2.x (Windows)
             import msvcrt
-            msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
-        return sys.stdout
+            msvcrt.setmode(stdio.fileno(), os.O_BINARY)
+        return stdio
     else:
-        # use 'buffer' attribute to write binary to stdout on python3.x
-        if hasattr(sys.stdout, 'buffer'):
-            return sys.stdout.buffer
+        # get 'buffer' attribute to read/write binary data on python3.x
+        if hasattr(stdio, 'buffer'):
+            return stdio.buffer
         else:
-            return sys.__stdout__.buffer
+            orig_stdio = getattr(sys, "__%s__" % stream)
+            return orig_stdio.buffer
 
 
-def get_binary_stdin():
-    if sys.version_info[0] < 3:
-        if sys.platform == 'win32':
-            # set stdin binary flag on python2.x (Windows)
-            import msvcrt
-            msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
-        return sys.stdin
+def decompress(data, output_length=None, max_bufsize=MAX_BUFFER_SIZE):
+    """ Decompress the data. Set decoded buffer size to output_length.
+    If output_length is None, try to calculate it using get_decompressed_size
+    method. If the latter fails, do the decompression in a loop, increasing
+    the buffer size until it reaches the 'max_bufsize' limit.
+    """
+    if output_length:
+        return brotli.decompress(data, output_length)
     else:
-        # use 'buffer' attribute to read binary from stdin on python3.x
-        if hasattr(sys.stdin, 'buffer'):
-            return sys.stdin.buffer
+        try:
+            output_length = brotli.get_decompressed_size(data)
+        except brotli.error as e:
+            bufsize = 5*len(data)
+            while bufsize < max_bufsize:
+                try:
+                    return brotli.decompress(data, bufsize)
+                except brotli.error:
+                    bufsize = bufsize*10
+            raise brotli.error("maximum buffer size reached")
         else:
-            return sys.__stdin__.buffer
+            return brotli.decompress(data, output_length)
 
 
 def main(args):
@@ -65,7 +81,7 @@ def main(args):
         if sys.stdin.isatty():
             # interactive console, just quit
             usage()
-        infile = get_binary_stdin()
+        infile = get_binary_stdio('stdin')
         data = infile.read()
 
     if options.outfile:
@@ -74,21 +90,13 @@ def main(args):
             sys.exit(1)
         outfile = open(options.outfile, "wb")
     else:
-        outfile = get_binary_stdout()
+        outfile = get_binary_stdio('stdout')
 
     try:
         if options.decompress:
-            bufsize = options.bufsize or 10*len(data)
-            try:
-                data = brotli.decompress(data, bufsize)
-            except brotli.error:
-                raise
+            data = decompress(data, options.length, options.bufsize)
         else:
             data = brotli.compress(data, options.mode, options.transform)
-            if outfile.isatty():
-                # print compressed binary data as hex strings
-                from binascii import hexlify
-                data = hexlify(data)
     except brotli.error as e:
         print('[ERROR] %s: %s' % (e, options.infile or 'sys.stdin'),
               file=sys.stderr)
@@ -101,9 +109,9 @@ def main(args):
 def parse_options(args):
     try:
         raw_options, dummy = getopt.gnu_getopt(
-            args, "?hdi:o:fm:tb:",
+            args, "?hdi:o:fm:tl:b:",
             ["help", "decompress", "input=", "output=", "force", "mode=",
-             "transform", "bufsize="])
+             "transform", "length=", "bufsize="])
     except getopt.GetoptError as e:
         print(e, file=sys.stderr)
         usage()
@@ -122,7 +130,10 @@ class Options(object):
         self.decompress = self.force = self.transform = False
         self.infile = self.outfile = None
         self.mode = BROTLI_MODES['text']
-        self.bufsize = None
+        # decompressed length (in bytes)
+        self.length = None
+        # max buffer size if decompressed length is unknown (default 1GB)
+        self.bufsize = MAX_BUFFER_SIZE
         for option, value in raw_options:
             if option in ("-h", "--help"):
                 print(__doc__ % (__version__))
@@ -144,8 +155,11 @@ class Options(object):
                 self.mode = BROTLI_MODES[value]
             elif option in ('-t', '--transform'):
                 self.transform = True
+            elif option in ('-l', '--length'):
+                self.length = int(value)
             elif option in ('-b', '--bufsize'):
                 self.bufsize = int(value)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
