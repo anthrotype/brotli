2bc2250c8ed9915d7a444b757ecd571fb6fc1c88
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-10-01 17:52:51 +0100

    move cffi files to separate subfolder

--- python/cffi/_brotli_build.py
+++ python/cffi/_brotli_build.py
@@ -16,8 +16,8 @@ static int do_decompress(uint8_t *input, size_t length,
 
 ffi.set_source("_brotli_cffi", """
 #include <memory>
-#include "../enc/encode.h"
-#include "../dec/decode.h"
+#include "../../enc/encode.h"
+#include "../../dec/decode.h"
 
 using namespace brotli;
 
@@ -53,23 +53,23 @@ static int do_decompress(uint8_t *input, size_t length,
 }
 
 """, source_extension='.cpp',
-     sources=["../enc/backward_references.cc",
-              "../enc/block_splitter.cc",
-              "../enc/brotli_bit_stream.cc",
-              "../enc/encode.cc",
-              "../enc/entropy_encode.cc",
-              "../enc/histogram.cc",
-              "../enc/literal_cost.cc",
-              "../enc/metablock.cc",
-              "../enc/static_dict.cc",
-              "../enc/streams.cc",
-              "../enc/utf8_util.cc",
-              "../dec/dictionary.c",
-              "../dec/bit_reader.c",
-              "../dec/decode.c",
-              "../dec/huffman.c",
-              "../dec/streams.c",
-              "../dec/state.c",
+     sources=["../../enc/backward_references.cc",
+              "../../enc/block_splitter.cc",
+              "../../enc/brotli_bit_stream.cc",
+              "../../enc/encode.cc",
+              "../../enc/entropy_encode.cc",
+              "../../enc/histogram.cc",
+              "../../enc/literal_cost.cc",
+              "../../enc/metablock.cc",
+              "../../enc/static_dict.cc",
+              "../../enc/streams.cc",
+              "../../enc/utf8_util.cc",
+              "../../dec/dictionary.c",
+              "../../dec/bit_reader.c",
+              "../../dec/decode.c",
+              "../../dec/huffman.c",
+              "../../dec/streams.c",
+              "../../dec/state.c",
     ])
 
 
