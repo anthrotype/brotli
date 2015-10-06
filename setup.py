e343d3b5bb227c77a93e2ff5511643897abf8e6e
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-10-06 12:19:23 +0100

    [setup.py] use `keywords_with_side_effects` function from cryptography setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
-from setuptools import setup, find_packages, Command
-from setuptools.command.build_ext import build_ext 
+from setuptools import setup, Command
+from setuptools.command.build_ext import build_ext
+from distutils.command.build import build
+from setuptools.command.install import install
+from setuptools.command.test import test
 import platform
 import sys
 import os
@@ -130,6 +133,158 @@ class BuildExt(build_ext):
             target_lang=language)
 
 
+def keywords_with_side_effects(argv):
+    """
+    Get a dictionary with setup keywords that (can) have side effects.
+
+    :param argv: A list of strings with command line arguments.
+    :returns: A dictionary with keyword arguments for the ``setup()`` function.
+
+    This setup.py script uses the setuptools 'setup_requires' feature because
+    this is required by the cffi package to compile extension modules. The
+    purpose of ``keywords_with_side_effects()`` is to avoid triggering the cffi
+    build process as a result of setup.py invocations that don't need the cffi
+    module to be built (setup.py serves the dual purpose of exposing package
+    metadata).
+
+    All of the options listed by ``python setup.py --help`` that print
+    information should be recognized here. The commands ``clean``,
+    ``egg_info``, ``register``, ``sdist`` and ``upload`` are also recognized.
+    Any combination of these options and commands is also supported.
+
+    This function is based on the `setup.py script`_ of cryptography, which in
+    turn was originally based on the `setup.py script`_ of SciPy (see
+    also the discussion in `pip issue #25`_).
+
+    .. _pip issue #25: https://github.com/pypa/pip/issues/25
+    .. _setup.py scripts:
+    ..    https://github.com/pyca/cryptography/blob/master/setup.py
+    ..    https://github.com/scipy/scipy/blob/master/setup.py
+    """
+    no_setup_requires_arguments = (
+        '-h', '--help',
+        '-n', '--dry-run',
+        '-q', '--quiet',
+        '-v', '--verbose',
+        '-V', '--version',
+        '--author',
+        '--author-email',
+        '--classifiers',
+        '--contact',
+        '--contact-email',
+        '--description',
+        '--egg-base',
+        '--fullname',
+        '--help-commands',
+        '--keywords',
+        '--licence',
+        '--license',
+        '--long-description',
+        '--maintainer',
+        '--maintainer-email',
+        '--name',
+        '--no-user-cfg',
+        '--obsoletes',
+        '--platforms',
+        '--provides',
+        '--requires',
+        '--url',
+        'clean',
+        'egg_info',
+        'register',
+        'sdist',
+        'upload',
+    )
+
+    def is_short_option(argument):
+        """Check whether a command line argument is a short option."""
+        return len(argument) >= 2 and argument[0] == '-' and argument[1] != '-'
+
+    def expand_short_options(argument):
+        """Expand combined short options into canonical short options."""
+        return ('-' + char for char in argument[1:])
+
+    def argument_without_setup_requirements(argv, i):
+        """Check whether a command line argument needs setup requirements."""
+        if argv[i] in no_setup_requires_arguments:
+            # Simple case: An argument which is either an option or a command
+            # which doesn't need setup requirements.
+            return True
+        elif (is_short_option(argv[i]) and
+              all(option in no_setup_requires_arguments
+                  for option in expand_short_options(argv[i]))):
+            # Not so simple case: Combined short options none of which need
+            # setup requirements.
+            return True
+        elif argv[i - 1:i] == ['--egg-base']:
+            # Tricky case: --egg-info takes an argument which should not make
+            # us use setup_requires (defeating the purpose of this code).
+            return True
+        else:
+            return False
+
+    if all(argument_without_setup_requirements(argv, i)
+           for i in range(1, len(argv))):
+        return {
+            "cmdclass": {
+                "build": DummyBuild,
+                "install": DummyInstall,
+                "test": DummyPyTest,
+            }
+        }
+    else:
+        cffi_modules = [
+            "python/build_brotli.py:ffi",
+        ]
+
+        return {
+            "setup_requires": requirements,
+            "cmdclass": {
+                "build_ext": BuildExt,
+                "test": TestCommand,
+            },
+            "cffi_modules": cffi_modules
+        }
+
+
+setup_requires_error = ("Requested setup command that needs 'setup_requires' "
+                        "while command line arguments implied a side effect "
+                        "free command or option.")
+
+
+class DummyBuild(build):
+    """
+    This class makes it very obvious when ``keywords_with_side_effects()`` has
+    incorrectly interpreted the command line arguments to ``setup.py build`` as
+    one of the 'side effect free' commands or options.
+    """
+
+    def run(self):
+        raise RuntimeError(setup_requires_error)
+
+
+class DummyInstall(install):
+    """
+    This class makes it very obvious when ``keywords_with_side_effects()`` has
+    incorrectly interpreted the command line arguments to ``setup.py install``
+    as one of the 'side effect free' commands or options.
+    """
+
+    def run(self):
+        raise RuntimeError(setup_requires_error)
+
+
+class DummyPyTest(test):
+    """
+    This class makes it very obvious when ``keywords_with_side_effects()`` has
+    incorrectly interpreted the command line arguments to ``setup.py test`` as
+    one of the 'side effect free' commands or options.
+    """
+
+    def run_tests(self):
+        raise RuntimeError(setup_requires_error)
+
+
 setup(
     name="Brotli",
     version=get_version(),
@@ -165,17 +320,13 @@ setup(
         ],
     package_dir={"": "python/lib"},
     packages=['brotli'],
-    ext_package='brotli',
+
     tests_require=requirements,
-    setup_requires=requirements,
     install_requires=requirements,
-    cffi_modules=["python/build_brotli.py:ffi"],
+
     zip_safe=False,
-    cmdclass={
-        'build_ext': BuildExt,
-        'test': TestCommand
-        },
     entry_points={
         'console_scripts': ["bro = brotli.bro:main"]
         },
+    **keywords_with_side_effects(sys.argv)
 )
